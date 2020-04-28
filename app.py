59a9958c48940cd7bf1056d69369edfa6cca20cd
Bo <bo@bracquez.be>
2020-04-28 15:09:12 +0200

    feat(app): integrate spec builder

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import libcloud.security
 from flask import Flask
 from flask_restful_swagger import swagger
 from flask_restplus import Api
+import inspect, libcloud
 
 # Create Flask app
 app = Flask(__name__)
@@ -16,3 +17,60 @@ app.run(debug=True)
 
 # Set SSL verification to off
 libcloud.security.VERIFY_SSL_CERT = False
+
+# Spec builder
+def build_specs():
+    specs = []
+
+    # Get all module members of libcloud with inspect
+    for providerBase in inspect.getmembers(libcloud, inspect.ismodule):
+        try:
+            # Try to import the found module
+            mdl = __import__("libcloud.%s" % providerBase[0], fromlist=["libcloud"])
+
+            # Import fails = skip, we should not process this item
+
+            # Try to address the two methods we need for creating the spec
+            mdl.types.Provider
+            mdl.providers.get_driver
+
+            # If we cannot address the methods (aka they are missing)
+            # Then this means that we are dealing with a module that is not a provider
+            # = skip
+
+            # Iterate over all the providers
+            for provider in mdl.types.Provider.__dict__:
+                # Check if the found member is private or not (prefixed with _)
+                if provider[:1] != "_":
+                    try:
+                        # Get the driver
+                        cls = mdl.providers.get_driver(getattr(mdl.types.Provider, provider))
+
+                        # List all methods
+                        for method in inspect.getmembers(cls, inspect.isfunction):
+                            # Set the current method to the method name (first entry of tupple)
+                            method = method[0]
+
+                            # Check if the found member is private or not (prefixed with _)
+                            if method[:1] != "_":
+                                # Create dictionary with info/data
+                                spec = {
+                                    "method": method,
+                                    "driver": providerBase[0],
+                                    "provider": provider,
+                                    "data": {},
+                                }
+
+                                # Iterate over all the function parameter names and give them a blank value
+                                for dataParam in [arg for arg in inspect.getfullargspec(getattr(cls, method)).args if
+                                                  arg != "self"]:
+                                    spec["data"][dataParam] = ""
+
+                                # Add to the specs
+                                specs.append(spec)
+                    except:
+                        pass
+        except:
+            pass
+
+    return specs
