9ba916cd078a2047a72d71fab73b3f7ad63d6ce5
Bo <bo@bracquez.be>
2020-04-28 15:08:37 +0200

    docs(app): basic docs

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ from flask import Flask
 from flask_restful_swagger import swagger
 from flask_restplus import Api
 
+# Create Flask app
 app = Flask(__name__)
 api = swagger.docs(Api(app), apiVersion='0.0.1',
                    resourcePath='/',
                    produces=["application/json", "text/html"],
                    api_spec_url='/api/spec',
                    description='REST Cloud API')
+
+# Set debugging to true
 app.run(debug=True)
 
+# Set SSL verification to off
 libcloud.security.VERIFY_SSL_CERT = False
