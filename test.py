81c4db8881f6d684fb2de0639df27bc18da5f36d
Bo <bo@bracquez.be>
2020-04-28 15:04:12 +0200

    feat(spec): module constraints

--- test.py
+++ test.py
@@ -10,6 +10,14 @@ for providerBase in inspect.getmembers(libcloud, inspect.ismodule):
 
         # Import fails = skip, we should not process this item
 
+        # Try to address the two methods we need for creating the spec
+        mdl.types.Provider
+        mdl.providers.get_driver
+
+        # If we cannot address the methods (aka they are missing)
+        # Then this means that we are dealing with a module that is not a provider
+        # = skip
+
     except:
         pass
 
